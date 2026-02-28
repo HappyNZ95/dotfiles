05fd4711c69928d74b4b447ab69d9e04a224ee00
Hayden Te Wini <htewini@gmail.com>
2026-02-28 21:22:21 +1300

    fixing smb share

--- helpers/create-hdd-fileshare.sh
+++ helpers/create-hdd-fileshare.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+sudo smbpasswd -a htw
 sudo tee -a /etc/samba/smb.conf >/dev/null <<'EOF'
 [hdd]
    path = /mnt/hdd
@@ -7,3 +8,5 @@ sudo tee -a /etc/samba/smb.conf >/dev/null <<'EOF'
    read only = no
    guest ok = yes
 EOF
+
+sudo systemctl restart smb
